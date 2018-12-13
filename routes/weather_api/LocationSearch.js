468cb8e31cf1b096d6727d06405d80cc510b9b09
Jhedi Ayala <jhedi@Jhedis-MacBook-Pro.local>
2018-12-13 17:49:54 -0500

    more routes and module-alias

--- routes/weather_api/LocationSearch.js
+++ routes/weather_api/LocationSearch.js
@@ -2,8 +2,9 @@ const fetch = require('node-fetch');
 const apiQuery = require('models').apiQuery;
 
 module.exports = (app) => {
+  
   app.post('/search-location-weather', (req, res) => {
-    const requestBody = req.body:
+    const requestBody = req.body;
     const apiUrl = apiQuery(requestBody.locationType, requestBody.locationData);
 
     fetch(apiUrl)
