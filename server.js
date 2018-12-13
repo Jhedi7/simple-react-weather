1af0c71bd881b60f3b6fad6c4adada1417dbafd5
Jhedi Ayala <jhedi@Jhedis-MacBook-Pro.local>
2018-12-13 14:19:47 -0500

    connected react and express

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-console.log('hi!')
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+
+
+
+app.get('/express_backend', (req, res) => {
+  res.send({ express: 'React and Express are connected!' });
+});
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => console.log(`Listening on port... ${port}`));
