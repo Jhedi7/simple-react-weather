80ae51bd79189c0a824bbc50f0220a8788879547
Jhedi Ayala <jhedi@Jhedis-MacBook-Pro.local>
2018-12-13 19:40:57 -0500

    react re-routing

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { App, ErrorDisplay } from './views/Components/index'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
