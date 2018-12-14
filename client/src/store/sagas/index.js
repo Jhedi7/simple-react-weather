b583e1925d6c8ae07cbfd96f21b1b2de6900f640
Jhedi Ayala <jhedi@Jhedis-MacBook-Pro.local>
2018-12-13 21:15:48 -0500

    store index

--- client/src/store/sagas/index.js
+++ client/src/store/sagas/index.js
@@ -4,5 +4,30 @@ import { apiQuery } from '../../../../models/apiQuery'
 
 export default function * root () {
   yield takeLatest(actions.GET_WEATHER_DATA, fetchWeatherData);
-  
+
+}
+
+function * fetchWeatherData(action) {
+  try {
+    const { weatherSearchData } = action.payload;
+
+    yield put({ type: actions.LOADING_WEATHER_DATA });
+
+    const response = yield call(weatherApiPromiseWrapper, weatherSearchData);
+
+    yield put({ type: actions.LOADED_WEATHER_DATA, payload: { ...response.data } });
+
+
+  } catch (e) {
+    yield put({ type: actions.ERROR_WEATHER_DATA });
+  }
+}
+
+function * weatherApiPromiseWrapper(weatherSearchData) {
+  const promise = yield new Promise((resolve, reject) => {
+    apiQuery({}, weatherSearchData, function(err, data) {
+      err ? reject : resolve
+    })
+  })
+  return promise;
 }
